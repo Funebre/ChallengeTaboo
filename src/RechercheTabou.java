1ddc897cab63132af4f15ddca961151c15edaf77
dixy <dixy@dicssy.net>
2012-05-31 16:33:06 +0200

    Second commit

---
+++ src/RechercheTabou.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ *     This file is part of ag41-print12-challenge.
+ *     
+ *     ag41-print12-challenge is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *     
+ *     ag41-print12-challenge is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *     
+ *     You should have received a copy of the GNU General Public License
+ *     along with ag41-print12-challenge.  If not, see <http://www.gnu.org/licenses/>.
+ *     
+ */
+
+import java.util.Arrays;
+import java.util.Random;
+
+
+public class RechercheTabou {
+    
+}
